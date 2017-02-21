2fc4c4374a6ec28023c5e0fa8c8bcefdf7c0ae82
tgarriz <tgarriz@gmail.com>
2017-02-21 16:00:25 -0300

    Agregado Compose

--- instalador_docker.sh
+++ instalador_docker.sh
@@ -13,4 +13,6 @@ add-apt-repository \
        main"
 apt-get update
 apt-get -y install docker-engine
+curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 apt-get -y upgrade
