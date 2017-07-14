2bbccca4fa71bd6d03435ef81619a1d2e55da1dd
tgarriz <tgarriz@gmail.com>
2017-07-14 17:11:16 -0300

    actualizado

--- Vagrantfile
+++ Vagrantfile
@@ -98,4 +98,11 @@ Vagrant.configure("2") do |config|
     #esclavo2.vm.provision "file", source:"~/lab/maestro/hosts", destination: "/etc/hosts"
   end
 
+  config.vm.define "esclavo3" do |esclavo3|
+    esclavo3.vm.hostname = "esclavo3"
+    esclavo3.vm.provision :shell, path: "instalador_docker.sh", privileged: true
+    esclavo3.vm.provision :shell, path: "set_hostname.sh", privileged: true
+    esclavo3.vm.network "private_network", ip: "10.10.0.253"
+  end
+
 end
--- instalador_docker.sh
+++ instalador_docker.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y curl vim nmap tree
 apt-get install -y apt-transport-https \
-                       ca-certificates \
-                       software-properties-common
-curl -fsSL https://yum.dockerproject.org/gpg | apt-key add -
-apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+	ca-certificates \
+        software-properties-common \
+	curl \
+	gnupg2 \
+	vim \
+	nmap \
+	tree
+		    
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       debian-$(lsb_release -cs) \
-       main"
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
 apt-get update
-apt-get -y install docker-engine
-curl -L "https://github.com/docker/compose/releases/download/1.11.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+apt-get -y install docker-ce
+curl -L https://github.com/docker/compose/releases/download/1.14.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 apt-get -y upgrade
